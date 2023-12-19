dec590103ce274ca9ac796a0f4403dbe7f4a583a
Krish Shah <shahkrish2016@gmail.com>
2023-12-18 19:09:56 -0500

    removed excess flask code

--- vocab_voyager/app.py
+++ vocab_voyager/app.py
@@ -4,18 +4,12 @@ from word import Word
 
 app = Flask(__name__)
 
-static_path = os.path.join(os.path.dirname(__file__), 'static')
-
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         return redirect(url_for('definition', word = request.form.get('query')))
     else:
-        return render_template("base.html", static_path = static_path)
+        return render_template("base.html")
 
 @app.route('/<word>/')
 def definition(word):
