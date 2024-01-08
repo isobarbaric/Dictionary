b9b1e780630d7be71b384fbe4eb8536bc9544f8f
Krish Shah <shahkrish2016@gmail.com>
2024-01-08 18:16:54 -0500

    css for navbar, figured out how to style crispy search bar

--- dictionary/forms.py
+++ dictionary/forms.py
@@ -1,13 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import VocabTerm
 
 class WordForm(forms.Form):
     word = forms.CharField(
         label = False,
         max_length = 50,
-        widget = forms.TextInput(attrs={'placeholder': 'Enter a word here'})
+        widget = forms.TextInput(attrs={'placeholder': 'Enter a word here', 'class': 'search-bar'})
     )
 
 class RegistrationForm(UserCreationForm):
